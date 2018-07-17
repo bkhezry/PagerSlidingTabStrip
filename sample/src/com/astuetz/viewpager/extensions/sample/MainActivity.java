8a09c0841bd5807e3a935e425fa41e896eb6b4b8
‌Behrouz <bkhezry@gmail.com>
2018-07-17 21:16:56 +0430

    add attr for support tab layout right side.

--- sample/src/com/astuetz/viewpager/extensions/sample/MainActivity.java
+++ sample/src/com/astuetz/viewpager/extensions/sample/MainActivity.java
@@ -27,7 +27,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.Menu;
@@ -41,122 +41,122 @@ import com.readystatesoftware.systembartint.SystemBarTintManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.tabs)
-    PagerSlidingTabStrip tabs;
-    @BindView(R.id.pager)
-    ViewPager pager;
+  @BindView(R.id.toolbar)
+  Toolbar toolbar;
+  @BindView(R.id.tabs)
+  PagerSlidingTabStrip tabs;
+  @BindView(R.id.pager)
+  ViewPager pager;
 
-    private MyPagerAdapter adapter;
-    private Drawable oldBackground = null;
-    private int currentColor;
-    private SystemBarTintManager mTintManager;
+  private MyPagerAdapter adapter;
+  private Drawable oldBackground = null;
+  private int currentColor;
+  private SystemBarTintManager mTintManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
-        // create our manager instance after the content view is set
-        mTintManager = new SystemBarTintManager(this);
-        // enable status bar tint
-        mTintManager.setStatusBarTintEnabled(true);
-        adapter = new MyPagerAdapter(getSupportFragmentManager());
-        pager.setAdapter(adapter);
-        tabs.setViewPager(pager);
-        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
-                .getDisplayMetrics());
-        pager.setPageMargin(pageMargin);
-        pager.setCurrentItem(1);
-        changeColor(ContextCompat.getColor(getBaseContext(), R.color.green));
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+    setSupportActionBar(toolbar);
+    // create our manager instance after the content view is set
+    mTintManager = new SystemBarTintManager(this);
+    // enable status bar tint
+    mTintManager.setStatusBarTintEnabled(true);
+    adapter = new MyPagerAdapter(getSupportFragmentManager());
+    pager.setAdapter(adapter);
+    tabs.setViewPager(pager);
+    final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+      .getDisplayMetrics());
+    pager.setPageMargin(pageMargin);
+    pager.setCurrentItem(1);
+    changeColor(ContextCompat.getColor(getBaseContext(), R.color.green));
 
-        tabs.setOnTabReselectedListener(new PagerSlidingTabStrip.OnTabReselectedListener() {
-            @Override
-            public void onTabReselected(int position) {
-                Toast.makeText(MainActivity.this, "Tab reselected: " + position, Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+    tabs.setOnTabReselectedListener(new PagerSlidingTabStrip.OnTabReselectedListener() {
+      @Override
+      public void onTabReselected(int position) {
+        Toast.makeText(MainActivity.this, "Tab reselected: " + position, Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_contact:
+        QuickContactFragment.newInstance().show(getSupportFragmentManager(), "QuickContactFragment");
         return true;
     }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void changeColor(int newColor) {
+    tabs.setBackgroundColor(newColor);
+    mTintManager.setTintColor(newColor);
+    // change ActionBar color just if an ActionBar is available
+    Drawable colorDrawable = new ColorDrawable(newColor);
+    Drawable bottomDrawable = new ColorDrawable(ContextCompat.getColor(getBaseContext(), android.R.color.transparent));
+    LayerDrawable ld = new LayerDrawable(new Drawable[] {colorDrawable, bottomDrawable});
+    if (oldBackground == null) {
+      getSupportActionBar().setBackgroundDrawable(ld);
+    } else {
+      TransitionDrawable td = new TransitionDrawable(new Drawable[] {oldBackground, ld});
+      getSupportActionBar().setBackgroundDrawable(td);
+      td.startTransition(200);
+    }
+
+    oldBackground = ld;
+    currentColor = newColor;
+  }
+
+  public void onColorClicked(View v) {
+    int color = Color.parseColor(v.getTag().toString());
+    changeColor(color);
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putInt("currentColor", currentColor);
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    currentColor = savedInstanceState.getInt("currentColor");
+    changeColor(currentColor);
+  }
+
+  public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    private final String[] TITLES = {"Content", "Trending", "Top Paid", "Top Free", "Top Grossing", "Top New Paid",
+      "Top New Free", "Trending"};
+
+    MyPagerAdapter(FragmentManager fm) {
+      super(fm);
+    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_contact:
-                QuickContactFragment.newInstance().show(getSupportFragmentManager(), "QuickContactFragment");
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void changeColor(int newColor) {
-        tabs.setBackgroundColor(newColor);
-        mTintManager.setTintColor(newColor);
-        // change ActionBar color just if an ActionBar is available
-        Drawable colorDrawable = new ColorDrawable(newColor);
-        Drawable bottomDrawable = new ColorDrawable(ContextCompat.getColor(getBaseContext(), android.R.color.transparent));
-        LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
-        if (oldBackground == null) {
-            getSupportActionBar().setBackgroundDrawable(ld);
-        } else {
-            TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
-            getSupportActionBar().setBackgroundDrawable(td);
-            td.startTransition(200);
-        }
-
-        oldBackground = ld;
-        currentColor = newColor;
-    }
-
-    public void onColorClicked(View v) {
-        int color = Color.parseColor(v.getTag().toString());
-        changeColor(color);
+    public CharSequence getPageTitle(int position) {
+      return TITLES[position];
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("currentColor", currentColor);
+    public int getCount() {
+      return TITLES.length;
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        currentColor = savedInstanceState.getInt("currentColor");
-        changeColor(currentColor);
+    public Fragment getItem(int position) {
+      return SuperAwesomeCardFragment.newInstance(position);
     }
-
-    public class MyPagerAdapter extends FragmentPagerAdapter {
-
-        private final String[] TITLES = {"Categories", "Home", "Top Paid", "Top Free", "Top Grossing", "Top New Paid",
-                "Top New Free", "Trending"};
-
-        MyPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return TITLES[position];
-        }
-
-        @Override
-        public int getCount() {
-            return TITLES.length;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return SuperAwesomeCardFragment.newInstance(position);
-        }
-    }
+  }
 }
